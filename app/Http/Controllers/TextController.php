f8f252aa60714007074bcbb4970aa7f3d89bf786
Hasan <hsmusayev@gmail.com>
2024-08-04 23:09:57 +0400

    simplified upload images without BASE64

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -40,33 +40,13 @@ class TextController extends Controller
                 $text->setTranslation('title', $locale, $translation['title']);
 
 
-                $dom = new \DomDocument();
+                    $dom = new \DomDocument();
+
                     @$dom->loadHTML(mb_convert_encoding($translation['content'], 'HTML-ENTITIES', 'UTF-8'));
 
                     $images = $dom->getElementsByTagName('img');
 
-                    foreach ($images as $k => $img) {
-                        $image_64 = $img->getAttribute('src');
-
-                        if (strpos($image_64, 'data:image/') === 0) {
-
-                        $extenion = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];
-
-                        $replace = substr($image_64, 0, strpos($image_64, ',') +1);
-
-                        $image = str_replace($replace, '', $image_64);
-                        $image = str_replace(' ', '+', $image);
-
-                        $image_name = Str::random(10). '.' .$extenion;
-
-                        $image_name = "/storage/uploads/" .  $image_name;
-                        $path = public_path() . $image_name;
-                        file_put_contents($path, base64_decode($image));
-
-                        $img->removeAttribute('src');
-                        $img->setAttribute('src', $image_name);
-                        }
-                    }
+                    $this->uploadImageWithoutBASE64($images);
 
                 $htmlContent = $dom->saveHTML();
 
@@ -87,40 +67,19 @@ class TextController extends Controller
         try {
 
             foreach ($request->input('translations', []) as $locale => $translation) {
-               $text_editor->setTranslation('title', $locale, $translation['title']);
+                $text_editor->setTranslation('title', $locale, $translation['title']);
 
+                    $dom = new \DomDocument();
 
-                $dom = new \DomDocument();
                     @$dom->loadHTML(mb_convert_encoding($translation['content'], 'HTML-ENTITIES', 'UTF-8'));
 
                     $images = $dom->getElementsByTagName('img');
 
-                    foreach ($images as $k => $img) {
-                        $image_64 = $img->getAttribute('src');
-
-                        if (strpos($image_64, 'data:image/') === 0) {
-
-                        $extenion = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];
-
-                        $replace = substr($image_64, 0, strpos($image_64, ',') +1);
-
-                        $image = str_replace($replace, '', $image_64);
-                        $image = str_replace(' ', '+', $image);
-
-                        $image_name = Str::random(10). '.' .$extenion;
-
-                        $image_name = "/storage/uploads/" .  $image_name;
-                        $path = public_path() . $image_name;
-                        file_put_contents($path, base64_decode($image));
-
-                        $img->removeAttribute('src');
-                        $img->setAttribute('src', $image_name);
-                        }
-                    }
+                    $this->uploadImageWithoutBASE64($images);
 
                 $htmlContent = $dom->saveHTML();
 
-               $text_editor->setTranslation('content', $locale, $htmlContent);
+                $text_editor->setTranslation('content', $locale, $htmlContent);
             }
 
            $text_editor->save();
@@ -144,5 +103,33 @@ class TextController extends Controller
     {
         $text_editor->delete();
         return redirect()->route('text-editor.index')->with('success', 'Text deleted successfully');
+    }
+
+    public function uploadImageWithoutBASE64($images){
+
+
+
+        foreach ($images as $k => $img) {
+            $image_64 = $img->getAttribute('src');
+
+            if (strpos($image_64, 'data:image/') === 0) {
+
+            $extenion = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];
+
+            $replace = substr($image_64, 0, strpos($image_64, ',') +1);
+
+            $image = str_replace($replace, '', $image_64);
+            $image = str_replace(' ', '+', $image);
+
+            $image_name = Str::random(10). '.' .$extenion;
+
+            $image_name = "/storage/uploads/" .  $image_name;
+            $path = public_path() . $image_name;
+            file_put_contents($path, base64_decode($image));
+
+            $img->removeAttribute('src');
+            $img->setAttribute('src', $image_name);
+            }
+        }
     }
 }
