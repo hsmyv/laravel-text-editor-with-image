97ddd2231e5c1f51b6824707fd47e93bf3ab605c
Hasan <hsmusayev@gmail.com>
2024-09-03 12:11:47 +0400

    Wrapped base64 line code

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -6,7 +6,7 @@ use App\Models\Language;
 use App\Models\Text;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-
+use Illuminate\Support\Facades\File;
 class TextController extends Controller
 {
     public function create()
@@ -38,19 +38,9 @@ class TextController extends Controller
             $text = new Text();
             foreach ($request->input('translations', []) as $locale => $translation) {
                 $text->setTranslation('title', $locale, $translation['title']);
+                $this->saveContentWihoutBASE64($text, $locale, $translation['content'] ?? '');
 
 
-                    $dom = new \DomDocument();
-
-                    @$dom->loadHTML(mb_convert_encoding($translation['content'], 'HTML-ENTITIES', 'UTF-8'));
-
-                    $images = $dom->getElementsByTagName('img');
-
-                    $this->uploadImageWithoutBASE64($images);
-
-                $htmlContent = $dom->saveHTML();
-
-                $text->setTranslation('content', $locale, $htmlContent);
             }
 
             $text->save();
@@ -68,18 +58,8 @@ class TextController extends Controller
 
             foreach ($request->input('translations', []) as $locale => $translation) {
                 $text_editor->setTranslation('title', $locale, $translation['title']);
+                $this->saveContentWihoutBASE64($text_editor, $locale, $translation['content'] ?? '');
 
-                    $dom = new \DomDocument();
-
-                    @$dom->loadHTML(mb_convert_encoding($translation['content'], 'HTML-ENTITIES', 'UTF-8'));
-
-                    $images = $dom->getElementsByTagName('img');
-
-                    $this->uploadImageWithoutBASE64($images);
-
-                $htmlContent = $dom->saveHTML();
-
-                $text_editor->setTranslation('content', $locale, $htmlContent);
             }
 
            $text_editor->save();
@@ -105,31 +85,46 @@ class TextController extends Controller
         return redirect()->route('text-editor.index')->with('success', 'Text deleted successfully');
     }
 
-    public function uploadImageWithoutBASE64($images){
 
+    function saveContentWihoutBASE64($model, $locale, $item){
+        //Modified by Hasan Musa to contribute SummerNote
+            $dom = new \DomDocument();
+                @$dom->loadHTML(mb_convert_encoding($item ?? ' ', 'HTML-ENTITIES', 'UTF-8'));
 
+                $images = $dom->getElementsByTagName('img');
 
-        foreach ($images as $k => $img) {
-            $image_64 = $img->getAttribute('src');
+                foreach ($images as $k => $img) {
+                    $image_64 = $img->getAttribute('src');
 
-            if (strpos($image_64, 'data:image/') === 0) {
+                    if (strpos($image_64, 'data:image/') === 0) {
 
-            $extenion = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];
+                    $extenion = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];
 
-            $replace = substr($image_64, 0, strpos($image_64, ',') +1);
+                    $replace = substr($image_64, 0, strpos($image_64, ',') +1);
 
-            $image = str_replace($replace, '', $image_64);
-            $image = str_replace(' ', '+', $image);
+                    $image = str_replace($replace, '', $image_64);
+                    $image = str_replace(' ', '+', $image);
 
-            $image_name = Str::random(10). '.' .$extenion;
+                    $image_name = Str::random(10). '.' .$extenion;
 
-            $image_name = "/storage/uploads/" .  $image_name;
-            $path = public_path() . $image_name;
-            file_put_contents($path, base64_decode($image));
+                    $filePath = storage_path('app/public/uploads');
 
-            $img->removeAttribute('src');
-            $img->setAttribute('src', $image_name);
-            }
-        }
+                    if (!File::exists($filePath)) {
+                        File::makeDirectory($filePath, 0755, true);
+                    }
+                    $image_name = "/storage/uploads/" .  $image_name;
+                    $path = public_path() . $image_name;
+                    file_put_contents($path, base64_decode($image));
+
+                    $img->removeAttribute('src');
+                    $img->setAttribute('src', $image_name);
+                    }
+                }
+
+            // Save the modified HTML content back to $description
+            $htmlContent = $dom->saveHTML();
+
+            // Update the translation with the modified HTML content
+            $model->setTranslation('content', $locale, $htmlContent);
     }
 }
